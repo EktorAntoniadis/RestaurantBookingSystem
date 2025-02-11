66226190a6c65d7a4b1f410034a1a8ca05e9ab49
EktorAntoniadis <117900460+EktorAntoniadis@users.noreply.github.com>
2025-02-11 20:06:11 +0200

    Προσθήκη 2 βιβλιοθηκών

---
+++ RestaurantBookingSystem.Operations/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantBookingSystem.Operations
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ RestaurantBookingSystem.Services/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantBookingSystem.Services
+{
+    public class Class1
+    {
+
+    }
+}
