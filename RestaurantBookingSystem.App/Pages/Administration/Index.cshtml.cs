fff8c789aca4fffd85abb34d5449e0b7a6cac237
EktorAntoniadis <117900460+EktorAntoniadis@users.noreply.github.com>
2025-03-15 16:17:36 +0200

    Προσθήκη κώδικα για την λίστα εστιατορίων

--- RestaurantBookingSystem.App/Pages/Administration/Index.cshtml.cs
+++ RestaurantBookingSystem.App/Pages/Administration/Index.cshtml.cs
@@ -1,31 +1,88 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RestaurantBookingSystem.Data.Models;
+using RestaurantBookingSystem.Operations.Pagination;
 using RestaurantBookingSystem.Operations.Repositories.Interfaces;
+using RestaurantBookingSystem.Repositories.Implementations;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace RestaurantBookingSystem.App.Pages.Administration
 {
     public class AdminPageModel : PageModel
     {
         private readonly IUserRepository _userRepository;
-        public AdminPageModel(IUserRepository userRepository)
+        private readonly IRestaurantRepository _restaurantRepository;
+        public AdminPageModel(
+            IUserRepository userRepository,
+            IRestaurantRepository restaurantRepository)
         {
             _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _restaurantRepository = restaurantRepository ?? throw new ArgumentNullException(nameof(restaurantRepository));
         }
+
         public string? ViewName { get; set; }
 
         public IEnumerable<Role> Roles { get; set; }
 
         public IEnumerable<Permission> Permissions { get; set; }
 
+        public PaginatedList<Restaurant> Restaurants { get; set; }
+
+        [FromQuery]
+        public string? Name { get; set; }
+
+        [FromQuery]
+        public string? Menu { get; set; }
+
+        [FromQuery]
+        public string? Address { get; set; }
+
+        [FromQuery]
+        public string? City { get; set; }
+
+        [FromQuery]
+        public string? Country { get; set; }
+
+        [FromQuery]
+        public string? BusinessRegNo { get; set; }
+
+        [FromQuery]
+        public string? PostalCode { get; set; }
+
+        [FromQuery]
+        public string? SortColumn { get; set; }
+
+        [FromQuery]
+        public string? SortDirection { get; set; }
+
+        [FromQuery]
+        public int PageIndex { get; set; } = 1;
+
         public IActionResult OnGet(string view)
         {
             ViewName = string.IsNullOrEmpty(view) ? "_Dashboard" : view;
 
-            if(view == "_RolesPermissions")
+            if (view == "_RolesPermissions")
             {
                 Roles = _userRepository.GetRoles();
                 Permissions = _userRepository.GetPermissions();
+            }
+
+            if (view == "_Restaurants")
+            {
+                var restaurants = _restaurantRepository.GetRestaurants(
+                    PageIndex,
+                    10,
+                    Name,
+                    Menu,
+                    Address,
+                    City,
+                    Country,
+                    BusinessRegNo,
+                    PostalCode,
+                    SortColumn,
+                    SortDirection
+                );
             }
 
             return Page();
