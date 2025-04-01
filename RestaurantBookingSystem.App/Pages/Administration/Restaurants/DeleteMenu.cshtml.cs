2c28a96c2ba2b95f2cf47d90c27148b73ea4ffc5
EktorAntoniadis <117900460+EktorAntoniadis@users.noreply.github.com>
2025-04-01 20:48:01 +0300

    Υλοποίηση σελίδας διαγραφής Menu

--- RestaurantBookingSystem.App/Pages/Administration/Restaurants/DeleteMenu.cshtml.cs
+++ RestaurantBookingSystem.App/Pages/Administration/Restaurants/DeleteMenu.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestaurantBookingSystem.Data.Models;
+using RestaurantBookingSystem.Operations.Repositories.Interfaces;
 
 namespace RestaurantBookingSystem.App.Pages.Administration.Restaurants
 {
     public class DeleteMenuModel : PageModel
     {
-        public void OnGet()
+        private readonly IRestaurantRepository _repository;
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public DeleteMenuModel(IRestaurantRepository repository)
         {
+            _repository = repository;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Restaurant = _repository.GetRestaurantById(id);
+
+            if (Restaurant == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int menuId)
+        {
+           _repository.DeleteMenu(menuId);
+            return RedirectToPage("/Administration/Index", new { view = "_Restaurants" });
         }
     }
 }
