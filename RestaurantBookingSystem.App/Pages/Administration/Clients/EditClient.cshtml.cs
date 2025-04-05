5aefb6206eee628bd215ca0a49408a552eedf75d
EktorAntoniadis <117900460+EktorAntoniadis@users.noreply.github.com>
2025-04-05 18:19:57 +0300

    Έναρξη υλοποίησης EditClient

--- RestaurantBookingSystem.App/Pages/Administration/Clients/EditClient.cshtml.cs
+++ RestaurantBookingSystem.App/Pages/Administration/Clients/EditClient.cshtml.cs
@@ -1,12 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestaurantBookingSystem.Data.Models;
+using RestaurantBookingSystem.Operations.Pagination;
+using RestaurantBookingSystem.Operations.Repositories.Interfaces;
 
 namespace RestaurantBookingSystem.App.Pages.Administration.Clients
 {
     public class EditClientModel : PageModel
     {
-        public void OnGet()
+        private readonly IUserRepository _userRepository;
+
+        private readonly IRestaurantRepository _restaurantRepository;
+        public EditClientModel(IUserRepository userRepository, IRestaurantRepository restaurantRepository)
         {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _restaurantRepository = restaurantRepository ?? throw new ArgumentNullException(nameof(restaurantRepository));
+        }
+
+        [BindProperty]
+
+        public RestaurantUser EditRestaurantOwner { get; set; }
+
+        [BindProperty]
+        public int? RestaurantId { get; set; }
+
+        [FromQuery]
+        public string Name { get; set; }
+
+        public PaginatedList<Restaurant> Restaurants { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            EditRestaurantOwner = _userRepository.GetRestaurantUserById(id);
+            RestaurantId = EditRestaurantOwner.RestaurantId;
+            return Page();
+        }
+
+        public IActionResult OnPostEditCurrentClient()
+        {
+            _userRepository.UpdateRestaurantUser(EditRestaurantOwner);
+            return RedirectToPage("/Administration/Index", new { view = "_Clients" });
         }
     }
 }
