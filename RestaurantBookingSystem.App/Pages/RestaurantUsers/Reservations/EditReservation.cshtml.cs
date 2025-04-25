4dceadc41e661e09972c1f9e688c295a84127c6f
EktorAntoniadis <117900460+EktorAntoniadis@users.noreply.github.com>
2025-04-25 20:53:56 +0300

    Απλη δημιουργία για κρατήσεις

---
+++ RestaurantBookingSystem.App/Pages/RestaurantUsers/Reservations/EditReservation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantBookingSystem.App.Pages.RestaurantUsers.Reservations
+{
+    public class EditReservationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
